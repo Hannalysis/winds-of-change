02221e63152fe5f16c910537250945e844ff5516
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2025-09-16 17:12:11 +0100

    chore: Edited variable names and tided up comments for init vis

--- explorations/highest-wind-speeds/hws_visualisation.py
+++ explorations/highest-wind-speeds/hws_visualisation.py
@@ -6,27 +6,24 @@ from pathlib import Path
 # Ensuring directory pathing is consistent
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
-# Build path to your CSV
 CSV_PATH = PROJECT_ROOT / "data" / "processed" / "queries" / "highest_wind_speeds.csv"
 
-# Load your wind data
+# Loading the wind data 
 df = pd.read_csv(CSV_PATH)
 
+# --- Streamlit layout ---
 st.title("~~~ Highest Wind Speed Map ~~~")
-st.markdown("Each point represents a town")
-
-# Layout: two columns
+st.markdown("Locations where wind speeds of 60km/h and above, occurenced more than once")
 col1, col2 = st.columns([4, 1]) 
 
-
-# Center the map on average lat/lon
+# --- Create the folium map ---
 map_center = [df["latitude"].mean(), df["longitude"].mean()]
 
-# Create folium map
 with col1:
     m = folium.Map(location=map_center, zoom_start=6.25)
 
-    def get_fill_colour(hws_count):
+    # Where the circle borders are darker for higher wind speed occurences
+    def get_outline_colour(hws_count):
         if hws_count == 2:
             return "gray"
         elif hws_count >= 3:
@@ -34,7 +31,8 @@ with col1:
         else:
             return "white"
         
-    def get_outline_colour(wind_speed):
+    # Where the inside circle fills are deeper blue for higher wind speeds   
+    def get_fill_colour(wind_speed):
         if wind_speed >= 64:
             return "navy"
         elif wind_speed >= 61:
@@ -43,8 +41,8 @@ with col1:
             return "teal"
 
     for _, row in df.iterrows():
-        fill_colour = get_fill_colour(row['high_wind_speed_count'])
-        outline_colour = get_outline_colour(row['highest_wind_speed'])
+        outline_colour = get_outline_colour(row['high_wind_speed_count'])
+        fill_colour = get_fill_colour(row['highest_wind_speed'])
         
         popup_text = f"""
         <strong>{row['location_name']}</strong><br>
@@ -54,30 +52,32 @@ with col1:
         folium.CircleMarker(
             location=[row['latitude'], row['longitude']],
             radius=5 + (row['highest_wind_speed'] * 1.5/ 5),
-            color=fill_colour,
+            color=outline_colour,
             fill=True,
-            fill_color=outline_colour,
+            fill_color=fill_colour,
             fill_opacity=0.7,
             popup=popup_text,
-            tooltip=folium.Tooltip(popup_text)
+            tooltip=folium.Tooltip(popup_text) # Location and wind speed on hover
         ).add_to(m)
 
     st_folium(m, width=675, height=600)
 
+# --- Create the folium map key ---
 with col2:
     st.markdown("""
     ** **            
     **Key**
     ** **
 
-    **Fill Color**  
+    **Fill Colour**  
     *Teal -> Navy*             
     (60+ km/h)
 
-    **Border Color**  
+    **Border Colour**  
     *Grey -> Black*  
     (2+ high wind speed occurences)
     ** **
     """)
 
+# Displaying the relevant columns of the table in streamlit
 st.dataframe(df[['location_name', 'highest_wind_speed', 'high_wind_speed_count']])
