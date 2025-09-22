d736456bb59e1fb050a65b2dc9d30b10592c68bf
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2025-09-22 15:21:56 +0100

    feat: Added basic validation null & dups on loc table

---
+++ data_quality/data_validation.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+# Grab local .env info
+load_dotenv()
+DB_URL = os.getenv("DB_URL")
+
+# Get tables
+engine = create_engine(DB_URL)
+locations_df = pd.read_sql_table('locations', engine)
+
+# --- Data Validation Functions --- #
+
+def check_nulls(df):
+    return df.isnull().sum()
+
+def check_duplicates(df):
+    return df.duplicated().sum()
+
+# -----------------------------------
+
+# Execute each validation for locations
+
+print("-" * 15)
+print("Locations table")
+print("-" * 15)
+print("🧪 Null Counts:")
+print(check_nulls(locations_df))
+
+print("\n🧪 Duplicate Rows:")
+print(f"Total duplicates: {check_duplicates(locations_df)}")
